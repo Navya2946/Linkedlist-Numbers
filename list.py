c480762afc2adec3286e57dca8c242e54c68e634
Navya2946 <kothapallinavyasri354@gmail.com>
2023-01-21 17:58:40 +0530

    Navy

---
+++ list.py
@@ -0,0 +1,92 @@
+# Python3 program to sort a linked list,
+# already sorted by absolute values
+	
+# Linked list Node
+class Node:
+	def __init__(self, d):
+		self.data = d
+		self.next = None
+
+class SortList:
+	def __init__(self):
+		self.head = None
+		
+	# To sort a linked list by actual values.
+	# The list is assumed to be sorted by
+	# absolute values.
+	def sortedList(self, head):
+		
+		# Initialize previous and
+		# current nodes
+		prev = self.head
+		curr = self.head.next
+		
+		# Traverse list
+		while(curr != None):
+			
+			# If curr is smaller than prev,
+			# then it must be moved to head
+			if(curr.data < prev.data):
+				
+				# Detach curr from linked list
+				prev.next = curr.next
+				
+				# Move current node to beginning
+				curr.next = self.head
+				self.head = curr
+				
+				# Update current
+				curr = prev
+			
+			# Nothing to do if current element
+			# is at right place
+			else:
+				prev = curr
+		
+			# Move current
+			curr = curr.next
+		return self.head
+	
+	# Inserts a new Node at front of
+	# the list
+	def append(self, new_data):
+		
+		# 1 & 2: Allocate the Node &
+		#	 Put in the data
+		new_node = Node(new_data)
+	
+		# 3. Make next of new Node as head
+		new_node.next = self.head
+	
+		# 4. Move the head to point to
+		# new Node
+		self.head = new_node
+	
+	# Function to print linked list
+	def printList(self, head):
+		temp = head
+		while (temp != None):
+			print(temp.data, end = " ")
+			temp = temp.next
+		print()
+	
+# Driver Code
+llist = SortList()
+
+# Constructed Linked List is
+# 1->2->3->4->5->6->7->8->8->9->null
+llist.append(-5)
+llist.append(-3)
+llist.append(-2)
+llist.append(1)
+llist.append(4)
+		
+print("Original List :")
+llist.printList(llist.head)
+		
+start = llist.sortedList(llist.head)
+
+print("Sorted list :")
+llist.printList(start)
+
+
---
+++ num.py
@@ -0,0 +1,8 @@
+a = 5
+b = 3
+result = a+b
+print(result)
+result = a-b
+print(result)
+result = a*b
+print(result)
